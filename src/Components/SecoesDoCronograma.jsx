ec0757afe7d830353c0722564b603c60bffd349f
amaralTheSage <amaraldesouza9@gmail.com>
2024-09-24 17:22:19 -0300

    💄 Increases the size of Cronograma images except speakers

--- src/Components/SecoesDoCronograma.jsx
+++ src/Components/SecoesDoCronograma.jsx
@@ -15,9 +15,9 @@ function SecoesDoCronograma({ assunto, reverse = false }) {
       >
         <div className="flex justify-center">
           <div className="relative">
-            <div className="bg-white w-[350px] aspect-video rounded-sm absolute top-3 right-3 "></div>
-            <div className="bg-orange-400 w-[350px] aspect-video rounded-sm absolute -top-3 left-3"></div>
-            <div className="w-[350px] aspect-video rounded-sm relative z-10">
+            <div className="bg-white w-[340px] sm:w-[400px] aspect-video rounded-sm absolute top-3 right-3 "></div>
+            <div className="bg-orange-400 w-[340px] sm:w-[400px] aspect-video rounded-sm absolute -top-3 left-3"></div>
+            <div className="w-[340px] sm:w-[400px] aspect-video rounded-sm relative z-10">
               <img
                 src="https://images.squarespace-cdn.com/content/v1/63f9284074ed022eb3ac8e91/4aadfae6-7772-4ffb-96ec-8d94a6a54487/D+Tas+Cover+Web.jpg?format=1000w"
                 alt=""

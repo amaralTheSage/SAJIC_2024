1bba10b60c20bc21ba41329059f842db19a46464
amaralTheSage <amaraldesouza9@gmail.com>
2024-09-24 20:25:14 -0300

    📱 Adds responsivity to 404 page

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -1,12 +1,12 @@
 function ErrorPage() {
   return (
     <div className="text-white flex h-screen w-screen items-center justify-center bg-[#0056B3]">
-      <div className="flex w-full flex-col items-center justify-center gap-8">
-        <h1 className="mb-12 select-none text-[20rem] font-black leading-none tracking-wider text-black">
+      <div className="flex m-auto flex-col items-center justify-center gap-8 w-4/5">
+        <h1 className="mb-12  text-[8rem] sm:text-[15rem] lg:text-[20rem] font-black leading-none tracking-wider text-black">
           404
         </h1>
 
-        <p>
+        <p className="text-center">
           A página que você tentou acessar não existe ou não foi encontrada.
         </p>
         <a
@@ -18,6 +18,6 @@ function ErrorPage() {
         </a>
       </div>
     </div>
-  )
+  );
 }
-export default ErrorPage
+export default ErrorPage;

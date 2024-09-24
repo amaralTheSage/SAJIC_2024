084991a3182bb4509973cf31df5579cd07d0d666
amaralTheSage <amaraldesouza9@gmail.com>
2024-09-24 12:52:40 -0300

    📱 Adds wrapping to footer navs

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
       className="bg-gradient-to-b from-[#004B9D] to-[#00254C] text-white p-24 flex flex-col items-start  gap-24
     "
     >
-      <div className="flex justify-between md:w-3/5 lg:w-2/5 w-5/6 ">
+      <div className="flex gap-8 flex-wrap md:flex-nowrap justify-between md:w-3/5 lg:w-2/5 w-5/6 ">
         <div>
           <h3 className="font-semibold text-lg mb-2 uppercase">Mapa do Site</h3>
           <nav className="flex flex-col gap-1">

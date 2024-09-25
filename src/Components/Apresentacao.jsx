618b779cd48724cc77c70dddd672e05a7b56863d
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2024-09-25 15:37:08 -0300

    :bug: fixing bug that causes background images don't show up in Vercel app

--- src/Components/Apresentacao.jsx
+++ src/Components/Apresentacao.jsx
@@ -1,6 +1,6 @@
 export default function Apresentacao(){
   return (
-    <section className="bg-[url('logo_senac_section2.png')] bg-cover h-[80vh] m-auto flex justify-center">
+    <section className="bg-[url('/logo_senac_section2.png')] bg-cover h-[80vh] m-auto flex justify-center">
       <div className="md:w-3/5 w-11/12 m-auto text-lg">
         <h2 className="uppercase font-semibold" >
           Sajic 4ª Edição | 17, 18 e 19 de outubro de 2024

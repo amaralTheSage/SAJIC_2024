4edbbcca90666576c95a150d7599ecf05aac9912
pedrohassen <pedro_hasse@yahoo.com.br>
2024-09-25 17:56:35 -0300

    📝 (Header.jsx): Update Header component to improve code readability and maintainability by refactoring NavLink classNames to use template literals for conditional styling based on pathname.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,44 +1,43 @@
-// import React from "react";
-
 import { NavLink, useLocation } from "react-router-dom";
 import { Menu } from "react-feather";
 import { useState } from "react";
 
 function Header() {
   const { pathname } = useLocation();
-
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="bg-[#313131]   text-white shadow-md">
-      <div className="flex gap-4 justify-between items-center sm:items-end  px-14">
+    <header className="bg-[#313131] text-white shadow-md">
+      <div className="flex gap-4 justify-between items-center sm:items-end px-14">
         <img src="LOGOBRANCA.png" alt="" className="w-24 my-2" />
 
         <div className="flex gap-4 max-md:items-center">
           <nav className="hidden sm:flex gap-4 h-full items-top">
             <NavLink
               to="/"
-              className={
-                pathname === "/" && "border-b-[12px] pb-2 border-white h-full"
-              }
+              className={`border-b-[12px] pb-2 h-full ${
+                pathname === "/" ? "border-white" : "border-transparent"
+              }`}
             >
               Home
             </NavLink>
             <NavLink
               to="/programacao"
-              className={
-                pathname === "/programacao" &&
-                "border-b-[12px] pb-2 border-white h-full"
-              }
+              className={`border-b-[12px] pb-2 h-full ${
+                pathname === "/programacao"
+                  ? "border-white"
+                  : "border-transparent"
+              }`}
             >
               Programação
             </NavLink>
             <NavLink
               to="/palestrantes"
-              className={
-                pathname === "/palestrantes" &&
-                "border-b-[12px] pb-2 border-white h-full"
-              }
+              className={`border-b-[12px] pb-2 h-full ${
+                pathname === "/palestrantes"
+                  ? "border-white"
+                  : "border-transparent"
+              }`}
             >
               Palestrantes
             </NavLink>
@@ -62,30 +61,28 @@ function Header() {
       </div>
 
       {isOpen && (
-        <nav className="sm:hidden flex  justify-evenly gap-3 h-full font-medium mt-2 items-top">
+        <nav className="sm:hidden flex justify-evenly gap-3 h-full font-medium mt-2 items-top">
           <NavLink
             to="/"
-            className={
-              pathname === "/" && "border-b-[12px] pb-2 border-white h-full"
-            }
+            className={`border-b-[12px] pb-2 h-full ${
+              pathname === "/" ? "border-white" : "border-transparent"
+            }`}
           >
             Home
           </NavLink>
           <NavLink
             to="/programacao"
-            className={
-              pathname === "/programacao" &&
-              "border-b-[12px] pb-2 border-white h-full"
-            }
+            className={`border-b-[12px] pb-2 h-full ${
+              pathname === "/programacao" ? "border-white" : "border-transparent"
+            }`}
           >
             Programacão
           </NavLink>
           <NavLink
             to="/palestrantes"
-            className={
-              pathname === "/palestrantes" &&
-              "border-b-[12px] pb-2 border-white h-full"
-            }
+            className={`border-b-[12px] pb-2 h-full ${
+              pathname === "/palestrantes" ? "border-white" : "border-transparent"
+            }`}
           >
             Palestrantes
           </NavLink>

458ccdcdc1c749e3aa5726c8e369eaba488ea190
amaralTheSage <amaraldesouza9@gmail.com>
2024-09-25 12:29:22 -0300

    📱 Adds fixed aspect ratio to image in CardPalestrante

--- src/Components/CardPalestrante.jsx
+++ src/Components/CardPalestrante.jsx
@@ -4,11 +4,11 @@ function CardPalestrante() {
   return (
     <div className="">
       <p>Data</p>
-      <div className="h-60">
+      <div className="">
         <img
           src="https://images.squarespace-cdn.com/content/v1/63f9284074ed022eb3ac8e91/4aadfae6-7772-4ffb-96ec-8d94a6a54487/D+Tas+Cover+Web.jpg?format=1000w"
           alt=""
-          className="h-full w-full object-cover rounded-sm"
+          className="aspect-[3/4] w-full object-cover rounded-sm"
         />
       </div>
       <div>

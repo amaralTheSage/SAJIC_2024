653a91003ac30bfdb0fbea7d9d4373a62fb06ae6
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2024-09-27 18:37:06 -0300

    :art: adding blankspaces between tags in ProgramacaoHorario

--- src/Components/ProgramacaoHorario.jsx
+++ src/Components/ProgramacaoHorario.jsx
@@ -1,16 +1,21 @@
 export default function ProgramacaoHorario({data, horarioCreden, turno, horarioCoffe}){
   return (
     <div className="w-4/5 m-auto flex flex-col items-center justify-center mt-32">
+
       <p className="font-bold text-8xl">{data}</p>
       <p className="text-6xl">{turno}</p>
+
       <div className="flex flex-col md:flex-row gap-8 justify-center text-center md:text-left mt-20 text-xl md:text-2xl">
+
         <div>
           <p className="font-bold">Credenciamento</p>
           <p className="font-light">
             Horário: <span className="font-bold">{horarioCreden}</span>
           </p>
         </div>
+
         <hr className="border-white w-20 md:-rotate-90 m-auto" />
+
         <div>
           <p className="font-bold">
             Coffee Break/Networking
@@ -19,6 +24,7 @@ export default function ProgramacaoHorario({data, horarioCreden, turno, horarioC
             Horário: <span className="font-bold">{horarioCoffe}</span>
           </p>
         </div>
+
       </div>
     </div>
   )

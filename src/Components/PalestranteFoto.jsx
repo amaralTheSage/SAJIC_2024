03afcbe63e777650faa591733bad3600a4fb6846
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2024-09-27 15:45:58 -0300

    :art: Minor fixes

--- src/Components/PalestranteFoto.jsx
+++ src/Components/PalestranteFoto.jsx
@@ -2,11 +2,11 @@ export default function PalestranteFoto({foto, nome, empresa, tema}){
   return(
     <figure className="flex justify-center items-center flex-col">
       <img src={foto} alt={nome} className="w-48 h-48 rounded-full object-cover"/>
-      <figcaption className="text-center font-light">
+      <figcaption className="text-center font-light pt-4">
         <ul>
-          <li><strong>{nome}</strong></li>
-          <li><span>{empresa}</span></li>
-          <li><span>{tema}</span></li>
+          <li><strong className="font-bold">{nome}</strong></li>
+          <li><span className="text-sm">{empresa}</span></li>
+          <li><span className="text-sm">{tema}</span></li>
         </ul>
       </figcaption>
     </figure>
--- src/pages/palestrantes/index.jsx
+++ src/pages/palestrantes/index.jsx
@@ -5,12 +5,12 @@ function Palestrantes() {
     <>
       <main>
 
-        <section>
+        <section className="py-10">
 
-          <h2 className="text-center font-bold md:text-5xl text-3xl">Palestrantes</h2>
-          <h3 className="text-center font-light md:text-xl text-lg">Tecnologia</h3>
+          <h2 className="text-center font-bold md:text-5xl text-3xl pb-20">Palestrantes</h2>
+          {/* <h3 className="text-center font-light md:text-xl text-lg">Tecnologia</h3> */}
 
-          <section className="flex flex-wrap justify-center py-10 gap-[10vw]">
+          <section className="flex flex-wrap justify-center py-10 gap-[8vw]">
             <PalestranteFoto
               foto="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/newscms/2019_05/2733926/190129-sherri-davidoff-white-hat-hacker-se-529p.jpg"
               nome="Um nome maior do que deveria"

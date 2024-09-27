73db56bdc149f33805b4924cbbe390df4d7b5e82
amaralTheSage <amaraldesouza9@gmail.com>
2024-09-27 16:18:09 -0300

    💄 Visually updates palestrantes page

--- src/Components/PalestranteFoto.jsx
+++ src/Components/PalestranteFoto.jsx
@@ -1,14 +1,24 @@
-export default function PalestranteFoto({foto, nome, empresa, tema}){
-  return(
+export default function PalestranteFoto({ foto, nome, empresa, tema }) {
+  return (
     <figure className="flex justify-center items-center flex-col">
-      <img src={foto} alt={nome} className="w-48 h-48 rounded-full object-cover"/>
+      <img
+        src={foto}
+        alt={nome}
+        className="w-full aspect-square rounded-full object-cover"
+      />
       <figcaption className="text-center font-light pt-4">
         <ul>
-          <li><strong className="font-bold">{nome}</strong></li>
-          <li><span className="text-sm">{empresa}</span></li>
-          <li><span className="text-sm">{tema}</span></li>
+          <li>
+            <strong className="font-bold">{nome}</strong>
+          </li>
+          <li>
+            <span className="text-sm">{empresa}</span>
+          </li>
+          <li>
+            <span className="text-sm">{tema}</span>
+          </li>
         </ul>
       </figcaption>
     </figure>
-  )
+  );
 }
--- src/pages/palestrantes/index.jsx
+++ src/pages/palestrantes/index.jsx
@@ -5,54 +5,65 @@ function Palestrantes() {
   return (
     <>
       <main>
-
-        <section className="py-10">
-
-          <h2 className="text-center font-bold md:text-5xl text-3xl pb-20">Palestrantes</h2>
+        <section className="py-20">
+          <h2 className="text-center font-bold md:text-4xl text-3xl">
+            Palestrantes
+          </h2>
           {/* <h3 className="text-center font-light md:text-xl text-lg">Tecnologia</h3> */}
 
-          <section className="flex flex-wrap justify-center py-10 gap-[8vw]">
+          <section className="w-2/3 sm:w-9/12 m-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-12 gap-[8vw] ">
             <PalestranteFoto
               foto="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/newscms/2019_05/2733926/190129-sherri-davidoff-white-hat-hacker-se-529p.jpg"
-              nome="Um nome maior do que deveria"
-              empresa="Empresa com nome grande"
-              tema="tema enorme jesus ahhhhhhhh"
+              nome="Pablo de Chiaro Rosa"
+              empresa="Unisenac"
+              tema="Inteligência Artificial"
             />
             <PalestranteFoto
               foto="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/newscms/2019_05/2733926/190129-sherri-davidoff-white-hat-hacker-se-529p.jpg"
-              nome="Um nome maior do que deveria"
-              empresa="Empresa com nome grande"
-              tema="tema enorme jesus ahhhhhhhh"
+              nome="Pablo de Chiaro Rosa"
+              empresa="Unisenac"
+              tema="Inteligência Artificial"
+            />
+
+            <PalestranteFoto
+              foto="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/newscms/2019_05/2733926/190129-sherri-davidoff-white-hat-hacker-se-529p.jpg"
+              nome="Pablo de Chiaro Rosa"
+              empresa="Unisenac"
+              tema="Inteligência Artificial"
+            />
+
+            <PalestranteFoto
+              foto="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/newscms/2019_05/2733926/190129-sherri-davidoff-white-hat-hacker-se-529p.jpg"
+              nome="Pablo de Chiaro Rosa"
+              empresa="Unisenac"
+              tema="Inteligência Artificial"
+            />
+
+            <PalestranteFoto
+              foto="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/newscms/2019_05/2733926/190129-sherri-davidoff-white-hat-hacker-se-529p.jpg"
+              nome="Pablo de Chiaro Rosa"
+              empresa="Unisenac"
+              tema="Inteligência Artificial"
             />
             <PalestranteFoto
               foto="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/newscms/2019_05/2733926/190129-sherri-davidoff-white-hat-hacker-se-529p.jpg"
-              nome="Um nome maior do que deveria"
-              empresa="Empresa com nome grande"
-              tema="tema enorme jesus ahhhhhhhh"
+              nome="Pablo de Chiaro Rosa"
+              empresa="Unisenac"
+              tema="Inteligência Artificial"
             />
+
             <PalestranteFoto
               foto="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/newscms/2019_05/2733926/190129-sherri-davidoff-white-hat-hacker-se-529p.jpg"
-              nome="Um nome maior do que deveria"
-              empresa="Empresa com nome grande"
-              tema="tema enorme jesus ahhhhhhhh"
+              nome="Pablo de Chiaro Rosa"
+              empresa="Unisenac"
+              tema="Inteligência Artificial"
             />
+
             <PalestranteFoto
               foto="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/newscms/2019_05/2733926/190129-sherri-davidoff-white-hat-hacker-se-529p.jpg"
-              nome="Um nome maior do que deveria"
-              empresa="Empresa com nome grande"
-              tema="tema enorme jesus ahhhhhhhh"
-            />
-            <PalestranteFoto
-              foto="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/newscms/2019_05/2733926/190129-sherri-davidoff-white-hat-hacker-se-529p.jpg"
-              nome="Um nome maior do que deveria"
-              empresa="Empresa com nome grande"
-              tema="tema enorme jesus ahhhhhhhh"
-            />
-            <PalestranteFoto
-              foto="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/newscms/2019_05/2733926/190129-sherri-davidoff-white-hat-hacker-se-529p.jpg"
-              nome="Um nome maior do que deveria"
-              empresa="Empresa com nome grande"
-              tema="tema enorme jesus ahhhhhhhh"
+              nome="Pablo de Chiaro Rosa"
+              empresa="Unisenac"
+              tema="Inteligência Artificial"
             />
           </section>
         </section>

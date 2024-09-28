be04c362eb435004543de2846d4d8c662ebdd86c
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2024-09-28 09:55:56 -0300

    :iphone: Making apresentacao Section even more responsive (on 4k screens)

--- src/Components/Apresentacao.jsx
+++ src/Components/Apresentacao.jsx
@@ -1,11 +1,13 @@
 export default function Apresentacao(){
   return (
-    <section className="bg-[url('/logo_senac_section2.png')] bg-cover h-[80vh] m-auto flex justify-center">
-      <div className="md:w-3/5 w-11/12 m-auto text-lg">
-        <h2 className="uppercase font-semibold" >
+    <section className="bg-[url('/logo_senac_section2.png')] bg-no-repeat bg-contain bg-center sm:h-screen h-[75vh]">
+
+      <article className="xl:px-[25vw] lg:px-[15vw] sm:px-[10vw] px-[5vw] gap-5 flex justify-center flex-col h-full">
+        <h2 className="uppercase font-semibold 2xl:text-2xl" >
           Sajic 4ª Edição | 17, 18 e 19 de outubro de 2024
         </h2>
-        <p>
+
+        <p className="font-light text-base 2xl:text-xl">
           A Semana Acadêmica do UniSenac é um evento que oferece uma série
           de atividades acadêmicas, culturais e profissionais, voltadas para
           o desenvolvimento de competências e a troca de conhecimentos entre
@@ -14,7 +16,8 @@ export default function Apresentacao(){
           como tecnologia, inovação, diversidade, empreendedorismo, e
           tendências de mercado.
         </p>
-      </div>
+      </article>
+
     </section>
   )
 }

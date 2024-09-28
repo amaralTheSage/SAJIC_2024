871b571c282199865c51937d3f6f0e07d7709f49
amaralTheSage <amaraldesouza9@gmail.com>
2024-09-28 15:17:03 -0300

    📱 Improved responsivity in CardPalestrante width

--- src/Components/CardPalestrante.jsx
+++ src/Components/CardPalestrante.jsx
@@ -8,7 +8,7 @@ function CardPalestrante({
   image = "https://images.squarespace-cdn.com/content/v1/63f9284074ed022eb3ac8e91/4aadfae6-7772-4ffb-96ec-8d94a6a54487/D+Tas+Cover+Web.jpg?format=1000w",
 }) {
   return (
-    <div className="embla_palestrantes_slide bg-green-600 object-cover aspect-video mx-6">
+    <div className="embla_palestrantes_slide  aspect-video mx-6 ">
       <p className="text-md md:text-[1.5vw] lg:text-[1.3vw] mb-1 w-fit">
         {data}
       </p>
@@ -19,10 +19,11 @@ function CardPalestrante({
           className="aspect-[3/4] object-cover rounded-sm border border-white"
         />
       </div>
-      <div className="bg-blue-300 ">
-        <p>{prof}</p>
-        <p>{empresa}</p>
-        <p>{tema}</p>
+      <div className="">
+        <p className="font-semibold capitalize text-lg">
+          {prof} - {empresa}
+        </p>
+        <p className="font-medium capitalize">{tema}</p>
       </div>
     </div>
   );

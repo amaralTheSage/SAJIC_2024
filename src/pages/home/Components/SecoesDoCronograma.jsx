6c2c6d74e789c2dd769a4107545982f7c150a405
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2024-10-02 19:07:05 -0300

    :adhesive_bandage: ordering palestrantes in home page

--- src/pages/home/Components/SecoesDoCronograma.jsx
+++ src/pages/home/Components/SecoesDoCronograma.jsx
@@ -69,26 +69,15 @@ function SecoesDoCronograma({ assunto, reverse = false, children }) {
           {/* TECNOLOGIA */}
           {assunto.toLowerCase() == "tecnologia" && (
             <CarrosselPalestrantes>
+
+
+              {/* 16/10 */}
               <CardPalestrante
-                prof="Gladimir Catarino"
-                data1="16/10, das 10:00 às 10:50"
-                empresa="UniSenac"
-                tema1="Internet of Things"
-                image="/palestrantes/gladLindo.jpg"
-              />
-              <CardPalestrante
-                data1="18/10, das 10:30 às 11:30"
-                prof="Pablo Chiaro de Rosa"
-                empresa="UniSenac"
-                tema1="Agentes de IA: aplicações além do Chat com LLM e Langchain"
-                image="/palestrantes/pablo.png"
-              />
-              <CardPalestrante
-                prof="Débora Carvalho"
-                data1="17/10, das 10:40 às 11:40"
-                empresa="UniSenac"
-                tema1="Jogos do Futuro: O Papel da Inteligência Artificial"
-                image="/palestrantes/debora_carvalho.jpeg"
+                prof="Ítalo Nolasco Ramos"
+                data1="16/10, das 09:20 às 11:40"
+                empresa="Nav9"
+                tema1="NextJS 14: Estrutura de uma aplicação profissional"
+                image="/palestrantes/italo_nolasco.jpeg"
               />
               <CardPalestrante
                 prof="William Machado"
@@ -98,13 +87,36 @@ function SecoesDoCronograma({ assunto, reverse = false, children }) {
                 tema1="As Implicações Éticas nas Tecnologias Digitais"
               />
               <CardPalestrante
+                prof="Gladimir Catarino"
+                data1="16/10, das 10:00 às 10:50"
+                empresa="UniSenac"
+                tema1="Internet of Things"
+                image="/palestrantes/gladLindo.jpg"
+              />
+
+
+              {/* 17/10 */}
+              <CardPalestrante
+                prof="Marcelo e Lorenzo"
+                data1="17/10, das 09:20 às 10:20"
+                empresa="UniSenac"
+                tema1="Github Pages: Montando e hospedando seu portifólio de graça com Github Pages"
+                image="/palestrantes/os_incel.jpg"
+              />
+              <CardPalestrante
+                prof="Débora Carvalho"
+                data1="17/10, das 10:40 às 11:40"
+                empresa="UniSenac"
+                tema1="Jogos do Futuro: O Papel da Inteligência Artificial"
+                image="/palestrantes/debora_carvalho.jpeg"
+              />
+              <CardPalestrante
                 prof="Thiago Nunes Batista"
                 data1="17/10, das 20:30 às 21:30"
                 empresa="ATLAS"
                 image="/palestrantes/tiago_batista.jpg"
                 tema1="Web Vitals: Como a experiência do usuário vai afetar o SEO do seu site?"
               />
-
               <CardPalestrante
                 prof="João Otávio"
                 data1="17/10, das 20:40 às 21:40"
@@ -113,6 +125,22 @@ function SecoesDoCronograma({ assunto, reverse = false, children }) {
                 image="/palestrantes/joao_otavio.jpg"
               />
 
+
+              {/* 18/10 */}
+              <CardPalestrante
+                prof="Marcelo Siedler"
+                data1="18/10, das 09:15 às 12:00"
+                empresa="IFSul"
+                tema1="Desenvolvimento de Jogos usando Unity"
+                image="/palestrantes/marcelo_siedler.png"
+              />
+              <CardPalestrante
+                data1="18/10, das 10:30 às 11:30"
+                prof="Pablo Chiaro de Rosa"
+                empresa="UniSenac"
+                tema1="Agentes de IA: aplicações além do Chat com LLM e Langchain"
+                image="/palestrantes/pablo.png"
+              />
               <CardPalestrante
                 data1="18/10, das 10:40 às 11:00"
                 prof="Eduardo Roveré"
@@ -120,22 +148,6 @@ function SecoesDoCronograma({ assunto, reverse = false, children }) {
                 tema1="Trilhando carreiras na tecnologia: Uma visão das práticas de sucesso do time Atlas"
                 image="/palestrantes/eduardo_atlas.jpg"
               />
-              <CardPalestrante
-                prof="Ítalo Nolasco Ramos"
-                data1="16/10, das 09:20 às 11:40"
-                empresa="Nav9"
-                tema1="NextJS 14: Estrutura de uma aplicação profissional"
-                image="/palestrantes/italo_nolasco.jpeg"
-              />
-
-              <CardPalestrante
-                prof="Marcelo e Lordenzo"
-                data1="17/10, das 09:20 às 10:20"
-                empresa="UniSenac"
-                tema1="Criação e Upload de Portifólio"
-                image="/palestrantes/os_incel.jpg"
-              />
-
               <CardPalestrante
                 prof="Augusto Rickes"
                 data1="18/10, das 19:15 às 20:15"
@@ -145,26 +157,15 @@ function SecoesDoCronograma({ assunto, reverse = false, children }) {
                 tema2="Desvendando o playwright: Automação moderna para testes Web"
                 data2="17/10, das 19:15 às 20:15"
               />
-              <CardPalestrante
-                prof="Marcelo Siedler"
-                data1="18/10, das 09:15 às 12:00"
-                empresa="IFSul"
-                tema1="Desenvolvimento de Jogos usando Unity"
-                image="/palestrantes/marcelo_siedler.png"
-              />
+
             </CarrosselPalestrantes>
           )}
           {/* GESTÃO */}
 
           {assunto.toLowerCase() == "gestão" && (
             <CarrosselPalestrantes gestao={true}>
-              <CardPalestrante
-                prof="Nanda Fersula"
-                data1="18/10, das 19:10 às 20:00"
-                empresa="Instituto Sigales"
-                image="/palestrantes/nanda_fersula.jpg"
-                tema1="Neuromarketing - Comece por Aqui"
-              />
+
+              {/* 16/10 */}
               <CardPalestrante
                 prof="Adriana Silva da Silva"
                 data1="16/10, das 10:40 às 11:40"
@@ -172,41 +173,9 @@ function SecoesDoCronograma({ assunto, reverse = false, children }) {
                 image="/palestrantes/adriana_silva.jpg"
                 tema1="Do Conceito ao Lançamento: Gestão Ágil de Projetos de Jogos Digitais"
               />
-              <CardPalestrante
-                data1="16/10, das 10:50 às 11:50"
-                prof="Eduardo Roveré"
-                empresa="ATLAS"
-                tema1="Mercado de Trabalho"
-                image="/palestrantes/eduardo_atlas.jpg"
-              />
-              <CardPalestrante
-                prof="Andréa Barros Augé"
-                tema1="Era hiperconectada: O Direito Digital e crimes digitais"
-                data1="18/10, das 20:30 às 21:30"
-                empresa=""
-                image="/palestrantes/andrea_auge.jpeg"
-              />
-              <CardPalestrante
-                prof="Régis Neves Machado"
-                data1="18/10, das 19:10 às 20:00"
-                empresa="Globo"
-                tema1="O Futuro da Carreira de TI"
-                image="/palestrantes/regis_machado.jpg"
-              />
-              <CardPalestrante
-                prof="Ítalo Nolasco Ramos"
-                data1="18/10, das 10:40 às 11:40"
-                empresa="Nav9"
-                tema1="Construindo e validando um MVP: Gerar valor mitigando riscos"
-                image="/palestrantes/italo_nolasco.jpeg"
-              />
-              <CardPalestrante
-                prof="Michele"
-                data1="16/10, das 10:40 às 11:40"
-                empresa="UniSenac"
-                tema1="Oficina de Currículo"
-                image="/palestrantes/"
-              />
+
+
+              {/* 17/10 */}
               <CardPalestrante
                 prof="Dalton Gaia da Silva"
                 data1="17/10, das 09:20 às 10:20"
@@ -220,6 +189,37 @@ function SecoesDoCronograma({ assunto, reverse = false, children }) {
                 empresa="ATLAS"
                 tema1="Debugando Hábitos"
                 image="/palestrantes/vitor_vitolla.jpeg"
+              />
+
+
+              {/* 18/10 */}
+              <CardPalestrante
+                prof="Ítalo Nolasco Ramos"
+                data1="18/10, das 10:40 às 11:40"
+                empresa="Nav9"
+                tema1="Construindo e validando um MVP: Gerar valor mitigando riscos"
+                image="/palestrantes/italo_nolasco.jpeg"
+              />
+              <CardPalestrante
+                prof="Nanda Fersula"
+                data1="18/10, das 19:10 às 20:00"
+                empresa="Instituto Sigales"
+                image="/palestrantes/nanda_fersula.jpg"
+                tema1="Neuromarketing - Comece por Aqui"
+              />
+              <CardPalestrante
+                prof="Régis Neves Machado"
+                data1="18/10, das 19:10 às 20:00"
+                empresa="Globo"
+                tema1="O Futuro da Carreira de TI"
+                image="/palestrantes/regis_machado.jpg"
+              />
+              <CardPalestrante
+                prof="Andréa Barros Augé"
+                tema1="Era hiperconectada: O Direito Digital e crimes digitais"
+                data1="18/10, das 20:30 às 21:30"
+                empresa=""
+                image="/palestrantes/andrea_auge.jpeg"
               />
               <CardPalestrante
                 prof="Eduardo Prestes"
